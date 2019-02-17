568611db1336fb603772da622d8eb5641bc68f60
Jared Gaston <jaredgaston68@gmail.com>
2019-02-17 17:19:49 -0600

    Forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,18 @@ urlpatterns = [
     path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowedbooks/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('books/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/',
+         views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/',
+         views.AuthorDelete.as_view(), name='author_delete'),
+    path('books/create/', views.BookCreate.as_view(), name='book_create'),
+    path('books/<int:pk>/update/',
+         views.BookUpdate.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/',
+         views.BookDelete.as_view(), name='book_delete'),
+
+
 ]
